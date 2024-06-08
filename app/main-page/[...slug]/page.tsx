b12a32db8ac7a35cec3030a92275ec199b9465a6
LuisKlopp <fbgus333@gmail.com>
2024-06-08 10:35:35 +0900

    Feat: console.log 추가

--- app/main-page/[...slug]/page.tsx
+++ app/main-page/[...slug]/page.tsx
@@ -6,6 +6,7 @@ export default function QuizDetailPage() {
   const pathName = usePathname();
   const parts = pathName.split("/");
   const questionId = Number(parts[parts.length - 1]);
+  console.log(questionId);
 
   return <div>main-page test</div>;
 }
