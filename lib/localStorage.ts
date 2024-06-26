898a7a886adaee57860279f703ce497cb116b5f7
LuisKlopp <fbgus333@gmail.com>
2024-06-27 02:02:40 +0900

    Feat: localstorage typeof 조건문 추가

--- lib/localStorage.ts
+++ lib/localStorage.ts
@@ -1,26 +1,34 @@
 import { QuestionType } from "@/types/question.types";
 
 export const getQuestions = () => {
-  const data = localStorage.getItem("questions");
-  return data ? JSON.parse(data) : null;
+  if (typeof window !== "undefined") {
+    const data = localStorage.getItem("questions");
+    return data ? JSON.parse(data) : null;
+  }
 };
 
 export const saveQuestions = (questions: QuestionType[]) => {
-  localStorage.setItem("questions", JSON.stringify(questions));
+  if (typeof window !== "undefined") {
+    localStorage.setItem("questions", JSON.stringify(questions));
+  }
 };
 
 export const getClickedQuestions = () => {
-  const data = localStorage.getItem("clickedQuestions");
-  return data ? JSON.parse(data) : [];
+  if (typeof window !== "undefined") {
+    const data = localStorage.getItem("clickedQuestions");
+    return data ? JSON.parse(data) : [];
+  }
 };
 
 export const saveClickedQuestion = (id: number) => {
-  const clickedQuestions = getClickedQuestions();
-  if (!clickedQuestions.includes(id)) {
-    clickedQuestions.push(id);
-    localStorage.setItem(
-      "clickedQuestions",
-      JSON.stringify(clickedQuestions),
-    );
+  if (typeof window !== "undefined") {
+    const clickedQuestions = getClickedQuestions();
+    if (!clickedQuestions.includes(id)) {
+      clickedQuestions.push(id);
+      localStorage.setItem(
+        "clickedQuestions",
+        JSON.stringify(clickedQuestions),
+      );
+    }
   }
 };
