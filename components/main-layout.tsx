086d9044e6e806ec55828336f334bdf9837a8bd1
LuisKlopp <fbgus333@gmail.com>
2024-06-28 19:10:16 +0900

    Style: scrollbar 제거

--- components/main-layout.tsx
+++ components/main-layout.tsx
@@ -4,7 +4,7 @@ interface MainLayoutProps {
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <main className="flex flex-col items-center justify-between bg-slate-300 h-[100dvh]">
+    <main className="flex h-[100dvh] flex-col items-center justify-between bg-slate-300 mdl:pl-40 mdl:pr-40">
       {children}
     </main>
   );
