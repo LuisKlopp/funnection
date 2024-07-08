49c65423b1d7fc7a253569e3078ca8ad8d38629e
LuisKlopp <fbgus333@gmail.com>
2024-07-08 11:02:50 +0900

    Feat: personal 페이지 숨기기

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex h-[100dvh] w-full flex-col items-center justify-center gap-10">
       <Link
-        href="/image-page"
+        href="/personal-page"
         className="cursor-default text-5xl text-slate-700 mdl:text-6xl"
       >
         Funnection
@@ -14,6 +14,7 @@ export default function Home() {
         <LinkButton href="/choice-page" title="OX 카드" />
         <LinkButton href="/question-page" title="단답 카드" />
         <LinkButton href="/balance-page" title="밸런스 카드" />
+        <LinkButton href="/image-page" title="이미지 카드" />
       </div>
     </div>
   );
