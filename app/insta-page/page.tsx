1b8b32db751be38f2cb5f90b2de5648dc02e53bf
LuisKlopp <fbgus333@gmail.com>
2024-07-20 15:01:21 +0900

    feat: 문구 변경

--- app/insta-page/page.tsx
+++ app/insta-page/page.tsx
@@ -5,7 +5,7 @@ export default async function InstaPage() {
   return (
     <div className="flex h-[100dvh] flex-col items-center justify-center gap-4">
       <h1 className="text-2xl font-medium text-slate-700 md:pb-10 mdl:text-4xl">
-        인스타 공유 합시다!
+        바로 맞팔 갑니다~
       </h1>
       <div className="flex flex-col gap-8">
         <span className="text-2xl">호스트 계정 :</span>
