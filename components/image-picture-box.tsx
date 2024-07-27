f6fa1b1ec2967c2d713e5f6f0ba1d6664de1ce73
LuisKlopp <fbgus333@gmail.com>
2024-07-27 15:36:18 +0900

    Style: 이미지 줄이기

--- components/image-picture-box.tsx
+++ components/image-picture-box.tsx
@@ -18,7 +18,7 @@ export const ImagePictureBox = ({
   const imageUrl = `${s3BaseUrl}/funnection-${userId}.webp`;
 
   return (
-    <div className="h-12 w-12 cursor-pointer font-medium transition-all duration-100 ease-in-out mdl:h-44 mdl:w-[130px] mdl:rounded-xl mdl:border-[3px] mdl:border-solid mdl:border-[#163b5e] mdl:shadow-[2px_3px_5px_rgb(43,43,43)]">
+    <div className="h-10 w-10 cursor-pointer font-medium transition-all duration-100 ease-in-out mdl:h-44 mdl:w-[130px] mdl:rounded-xl mdl:border-[3px] mdl:border-solid mdl:border-[#163b5e] mdl:shadow-[2px_3px_5px_rgb(43,43,43)]">
       <Image
         className="h-full w-full rounded-lg"
         alt="default-image"
